3e1e8c1f0d12f48c33d98c672aac3f7826ebc040
Priyanshu Ashiya <priyanshu2000ashiya@gmail.com>
2023-12-22 11:18:08 +0530

    added log sttement

--- src/main/java/com/example/POCApp/HelloController.java
+++ src/main/java/com/example/POCApp/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.POCApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET, value = "/api/pocgic")
     public String sayHello() {
-
+            log.info("generating welcome message");
         return " Welcome  -- I am here to give you a short POC of EKS for GIC ";
 
     }
